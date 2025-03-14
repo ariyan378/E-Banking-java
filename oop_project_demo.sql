5b5177d194bfc841793daf15510d2ae8138c3e22
Ariyan Islam hridoy <161193575+ariyan378@users.noreply.github.com>
2025-03-15 01:20:27 +0600

    Add files via upload

---
+++ oop_project_demo.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS ecommerce;
+USE ecommerce;
+
+CREATE TABLE Users (
+    phone_number VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    balance DECIMAL(10,2) DEFAULT 0.0
+);
+
+CREATE TABLE Transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sender_phone VARCHAR(20),
+    receiver_phone VARCHAR(20) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_phone) REFERENCES Users(phone_number),
+    FOREIGN KEY (receiver_phone) REFERENCES Users(phone_number)
+);
+
+INSERT INTO Users (phone_number, name, password, balance)
+VALUES 
+    ('1111111111', 'Ariyan', 'ariyanpass', 1000.00),
+    ('2222222222', 'Mohtasir', 'mohtasirpass', 1500.00),
+    ('3333333333', 'Shisita', 'shisitapass', 2000.00),
+    ('4444444444', 'Tasfin', 'tasfinpass', 1200.00),
+    ('5555555555', 'Dishad', 'dishadpass', 800.00);
+    
+SELECT * FROM Users ;
+SELECT * FROM Transactions ;
